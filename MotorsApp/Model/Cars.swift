02922cae1cbfba9393d646a8040c2244dc22e7b4
Stephen Clark <stephen.clark2@city-holdings.co.uk>
2022-05-28 14:13:54 +0100

    Add model object for JSON result

---
+++ MotorsApp/Model/Cars.swift
@@ -0,0 +1,19 @@
+//
+//  Cars.swift
+//  MotorsApp
+//
+//  Created by Stephen Clark on 28/05/2022.
+//
+
+import Foundation
+
+// MARK: - Cars
+struct Cars: Codable {
+    let searchResults: [SearchResult]
+}
+
+// MARK: - SearchResult
+struct SearchResult: Codable {
+    let id, name, title, make: String
+    let model, year, price: String
+}
